7cd9fec100621845e67bc8cee216c88bb063d2dc
zentrumpet <137319951+zentrumpet@users.noreply.github.com>
2023-06-29 13:32:38 -0700

    Add files via upload

---
+++ c++/hello/hello.cpp
@@ -0,0 +1,17 @@
+// C++ Hello World with initials
+
+#include <iostream>
+
+int main() {
+	std::cout << "\n";
+	std::cout << "Hello World = Hola Mundo!\n\n";
+
+	std::cout << " MMMM    MMMM  WW      WW      WW  BBBBB    \n";
+	std::cout << " MM MM  MM MM   WW    WWWW    WW   BB   BB  \n";
+	std::cout << " MM   MM   MM    WW  WW  WW  WW    BB BB    \n";
+	std::cout << " MM        MM     WWWW    WWWW     BB   BB  \n";
+	std::cout << " MM        MM      WW      WW      BBBBB    \n";
+	std::cout << "\n";
+    
+    return 0;
+}
